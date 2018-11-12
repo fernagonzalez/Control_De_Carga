2e8d2b80a120d4c141d00970315f32de9f05e3d2
fernagonzalez <fernandogonzalez393@gmail.com>
2018-11-12 07:22:13 -0300

    Update Estimacion_Primaria_SOC_Con_Tension.ino

--- Estimacion_Primaria_SOC_Con_Tension.ino
+++ Estimacion_Primaria_SOC_Con_Tension.ino
@@ -3,7 +3,7 @@ void Estimacion_Primaria_SOC_Con_Tension()
 {
 // Polinomio calculado con MATLAB, que nos da el porcentaje de descarga
 // en función de la tensión en bornes de la batería.
-
+x = V_bateria;
 Porcentaje_Descarga = 1.0e+09 * (-0.0010*x^5 + 0.0131*x^4 - 0.1150*x^3 + 0.6313*x^2 - 1.9766*x + 2.7039);
 // Donde x es la tensión en bornes de la bateria.
 float SOC_Inicial = SOC_Ultimo - Porcentaje_Descarga;
